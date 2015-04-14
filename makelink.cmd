c037987ebbce9be1ee047c65fdd2bcbe3494bb82
OGMalin <oddg@sjakk.biz>
2015-04-14 10:06:15 +0200

    Init

---
+++ makelink.cmd
@@ -0,0 +1,4 @@
+set JoomlaDir=C:\xampp\htdocs\joomladev
+mklink /J %JoomlaDir%\administrator\components\com_polartour administrator\components\com_polartour
+mklink /J %JoomlaDir%\components\com_polartour components\com_polartour
+mklink /J %JoomlaDir%\media\com_polartour media\com_polartour
